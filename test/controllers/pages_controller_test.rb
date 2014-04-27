12b05068d197afcb9109f8499646f475f08b77a7
luba <levoenm@gmail.com>
2014-04-27 00:48:42 -0400

    Add Pages

---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get landing" do
+    get :landing
+    assert_response :success
+  end
+
+  test "should get help" do
+    get :help
+    assert_response :success
+  end
+
+end
